22727b1dad8ae940225811281c15560ca4af395c
Paul Annekov <paul.annekov@gmail.com>
2017-03-15 00:39:38 +0200

    switched from netwatch to ip scan, fixing

--- device_tracker/mikrotik_netwatch.py
+++ device_tracker/mikrotik_netwatch.py
@@ -9,28 +9,31 @@ import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.device_tracker import (
-    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
+    DOMAIN, PLATFORM_SCHEMA, DeviceScanner, CONF_SCAN_INTERVAL)
 from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
-from homeassistant.const import CONF_HOSTS
 
 REQUIREMENTS = ['routeros-api==0.14']
 
 CONF_PORT = 'port'
+CONF_ADDRESS_RANGE = 'address_range'
+CONF_INTERFACE = 'interface'
 
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_HOST, default='192.168.88.1'): cv.string,
-    vol.Required(CONF_HOSTS): cv.ensure_list,
+    vol.Optional(CONF_ADDRESS_RANGE): cv.string,
+    vol.Optional(CONF_INTERFACE): cv.string,
     vol.Optional(CONF_PORT, default=8728): cv.string,
-    vol.Optional(CONF_PASSWORD, default='admin'): cv.string,
-    vol.Optional(CONF_USERNAME, default=''): cv.string,
+    vol.Optional(CONF_USERNAME, default='admin'): cv.string,
+    vol.Optional(CONF_PASSWORD, default=''): cv.string,
 })
 
 
 def get_scanner(hass, config):
     """Validate the configuration and return MTikScanner."""
     scanner = MikrotikDeviceScanner(config[DOMAIN])
+    _LOGGER.debug('Mikrotik init')
     return scanner if scanner.success_init else None
 
 
@@ -41,71 +44,92 @@ class MikrotikDeviceScanner(DeviceScanner):
         """Initialize the scanner."""
         self.last_results = None
         self.host = config[CONF_HOST]
-        self.hosts = config[CONF_HOSTS]
+        self.interface = config[CONF_INTERFACE]
+        self.address_range = config[CONF_ADDRESS_RANGE]
         self.port = config[CONF_PORT]
         self.username = config[CONF_USERNAME]
         self.password = config[CONF_PASSWORD]
+        self.scan_interval = config[CONF_SCAN_INTERVAL]
         self.success_init = True
 
         from routeros_api import RouterOsApiPool
 
         # Establish a connection to the Mikrotik router.
-        try:
-            connection = RouterOsApiPool(self.host, username=self.port, password=self.password, port=self.port)
-            self.client = connection.get_api()
-            self.leases = self.client.get_resource('/ip/dhcp-server/lease').get()
-            if self.leases == {}:
-                self.connected = True
-        except (ValueError, TypeError):
-            self.client = None
+        connection = RouterOsApiPool(self.host, username=self.username, password=self.password, port=self.port)
+        self.client = connection.get_api()
+        self.leases = self.client.get_resource('/ip/dhcp-server/lease').get()
+        _LOGGER.debug('Got leases %s', str(self.leases))
 
         # At this point it is difficult to tell if a connection is established.
         # So just check for null objects.
-        if self.client is None or not self.connected:
+        if not connection.connected:
             self.success_init = False
 
         if self.success_init:
             _LOGGER.info('Successfully connected to Mikrotik device')
+
+            # Reserve 2 seconds for API communication.
+            self.ip_scan_args = {"duration": str(max(self.scan_interval.seconds - 2, 1))}
+            if self.address_range:
+                self.ip_scan_args["address-range"] = self.address_range
+            if self.interface:
+                self.ip_scan_args["interface"] = self.interface
+            _LOGGER.debug('ip_scan_args %s', str(self.ip_scan_args))
+
             self._update_info()
         else:
             _LOGGER.error('Failed to establish connection to Mikrotik device with IP: %s', self.host)
 
-    # TODO: is there a way to throw exception and make HA don't call these methods?
     def scan_devices(self):
         self._update_info()
-        active_hosts = []
+        active_hosts = set()
         if self.last_results is None:
-            return active_hosts
-        for device in self.hosts:
-            ip = self.hosts[device]
-            dev = next((item for item in self.last_results if item['host'] == ip), None)
-            if dev is None:
-                _LOGGER.error('IP %s declared in "hosts", but not present in Tools - Netwatch', ip)
-                continue
-            lease = next((item for item in self.leases if item['address'] == dev['host']), None)
-            if lease is not None:
-                if dev['status'] == 'up':
-                    active_hosts.append(lease["mac-address"])
+            return []
+        for device in self.last_results:
+            mac = None
+            if 'mac-address' in device:
+                mac = device['mac-address']
             else:
-                _LOGGER.error('IP %s declared in "hosts", but not present in DHCP Server - Leases. Needed to get MAC')
-        return active_hosts
+                lease = next((item for item in self.leases if item['address'] == device['address']), None)
+                if lease is not None:
+                    mac = lease["mac-address"]
+            # TODO: last_results can contain multiple records for same IP and some can be w/ MAC, fix this
+            if mac is None:
+                _LOGGER.error('Can\'t define %s IP mac address, neither ip-scan, nor DHCP Server - Leases has this '
+                              'info.', device['address'])
+            else:
+                active_hosts.add(mac)
+        _LOGGER.debug('active_hosts %s', str(active_hosts))
+        return list(active_hosts)
 
     def get_device_name(self, mac):
-        if not self.success_init:
+        _LOGGER.debug('get_device_name "%s" "%s" "%s"', str(mac), str(self._name_from_ip_scan(mac)),
+                      str(self._name_from_leases(mac)))
+        return self._name_from_ip_scan(mac) or self._name_from_leases(mac)
+
+    def _name_from_ip_scan(self, mac):
+        priorities = ['netbios', 'dns', 'snmp']
+        for priority in priorities:
+            res = next((rec for rec in self.last_results if 'mac-address' in rec and rec['mac-address'] == mac and
+                        rec[priority]), None)
+            if res:
+                return res[priority]
+        return None
+
+    def _name_from_leases(self, mac):
+        lease = next((rec for rec in self.leases if rec['mac-address'] == mac), None)
+        if not lease:
             return None
-        lease = next((item for item in self.last_results if item['mac-address'] == mac), None)
-        if "comment" in lease:
+        if 'comment' in lease:
             return lease['comment']
-        elif "host-name" in lease and lease["host-name"] != {}:
+        elif 'host-name' in lease and lease['host-name'] != {}:
             return lease['host-name']
         else:
             return None
 
     def _update_info(self):
         """Retrieve latest information from the Mikrotik box."""
-        if not self.success_init:
-            return False
-
         _LOGGER.info('Polling')
-        self.last_results = self.client.get_resource('/tool/netwatch').get()
+        self.last_results = self.client.get_resource('/tool').call('ip-scan', arguments=self.ip_scan_args)
+        _LOGGER.info('results %s', str(self.last_results))
         return True
